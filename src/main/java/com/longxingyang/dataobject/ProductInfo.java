5479762144cf632b631187d7aa1d318a1fdad053
longxingyang <longxingyang@outlook.com>
2018-01-12 12:26:13 +0800

    1

---
+++ src/main/java/com/longxingyang/dataobject/ProductInfo.java
@@ -0,0 +1,47 @@
+package com.longxingyang.dataobject;
+
+import lombok.Data;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * 商品
+ * Created by a4420 on 17/11/25.
+ */
+@Entity
+@Data
+public class ProductInfo {
+
+    @Id
+    private String productId;
+
+    //名字
+    private String productName;
+
+    //单价
+    private BigDecimal productPrice;
+
+    //库存
+    private Integer productStock;
+
+    //商品小图
+    private String productIcon;
+
+    //商品类目
+    private Integer gategoryType;
+
+    //商品描述
+    private String productDescription;
+
+    //商品状态 0正常1下架
+    private Integer productStatus;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+}
---
+++ src/main/java/com/longxingyang/service/ProductService.java
@@ -0,0 +1,29 @@
+package com.longxingyang.service;
+
+import com.longxingyang.dataobject.ProductInfo;
+import com.longxingyang.dto.CartDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+/**
+ * Created by a4420 on 17/11/25.
+ */
+public interface ProductService {
+
+    ProductInfo findOne(String productId);
+
+    //查询在架商品
+    List<ProductInfo> findUpAll();
+
+    Page<ProductInfo> findAll(Pageable pageable);
+
+    ProductInfo save(ProductInfo productInfo);
+
+    //加库存
+    void increaseStock(List<CartDTO> cartDTOList);
+
+    //减库存
+    void decreaseStock(List<CartDTO> cartDTOList);
+}
